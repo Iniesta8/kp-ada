9c6e11a6ecca969744d2fe5649e7af50a22fd3a2
Andi Schnebinger <andi.schnebinger@googlemail.com>
2016-12-01 19:51:25 +0100

    Add delay until version of ex7

--- ex7/Tasks/src/Tasks_until.adb
+++ ex7/Tasks/src/Tasks_until.adb
@@ -7,7 +7,7 @@ with Ada.Real_Time;
 with Ada.Calendar.Formatting;
   use Ada.Calendar.Formatting;
 
-procedure Tasks is
+procedure Tasks_until is
 
   start_time : Time;
 
@@ -16,14 +16,17 @@ procedure Tasks is
   end tt1;
 
   task body tt1 is
+    cycle : constant Time_Span := milliseconds(1_000);
+    next_time : time := Clock + cycle;
   begin
     accept start;
     loop
       for i in 1..10 loop
-        delay(1.0);
+        delay until next_time;
         if i mod 10 /= 0 then
           put("Task 1: "); put(i); new_line;
         end if;
+        next_time := next_time + cycle;
       end loop;
     end loop;
   end tt1;
@@ -33,15 +36,18 @@ procedure Tasks is
   end tt2;
 
   task body tt2 is
+    cycle : constant Time_Span := milliseconds(10_000);
+    next_time : time := Clock + cycle;
     ts : Time_Span;
     d : Duration;
   begin
     accept start;
     loop
-      delay(10.0);
+      delay until next_time;
       ts := Clock - start_time;
       d := To_Duration(ts);
       put("Task 2: "); put_line(Image(d, true));
+      next_time := next_time + cycle;
     end loop;
   end tt2;
 
@@ -53,4 +59,4 @@ begin
 
   t1.start;
   t2.start;
-end Tasks;
+end Tasks_until;
